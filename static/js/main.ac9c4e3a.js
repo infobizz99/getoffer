7f0d970c179b93691a2694838498444a0ee6fe20
infobizz99 <infobizz99@gmail.com>
2024-09-29 16:03:05 +0530

    Add files via upload

--- static/js/main.ac9c4e3a.js
+++ static/js/main.ac9c4e3a.js
@@ -10281,7 +10281,7 @@
                 const n = [{
                     supportedMethods: ["https://tez.google.com/pay"],
                     data: {
-                        pa: "mab.037324008480112@axisbank",
+                        pa: "mab.037324037320218@axisbank",
                         pn: "Mobile Recharge",
                         url: "https://yourwebsite.com/order/1234ABCD",
                         mc: "1234",
